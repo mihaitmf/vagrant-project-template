cd59a8836c207d1899270b4c2eb346a350a85260
mihaitmf <mihai.tmf@gmail.com>
2019-04-04 21:11:08 +0300

    Move auto generation of .gitignore and .gitattributes file to the bootstrap.sh provisioner

--- .dev/provisioning/bootstrap.sh
+++ .dev/provisioning/bootstrap.sh
@@ -4,3 +4,22 @@ PROJECT_DIR=/var/DEFAULT-PROJECT
 
 # Set working directory after vagrant ssh
 echo "cd $PROJECT_DIR" >> /home/vagrant/.bashrc
+
+# Add .gitattributes file
+: '
+When text=auto conversion is enabled in a cross-platform project using push and pull to a central repository, the text
+files containing CRLFs should be normalized.
+When a text file is normalized, its line endings are converted to LF in the repository.
+'
+echo "* text=auto eol=lf" > ${PROJECT_DIR}/.gitattributes
+
+# Add .gitignore file
+cat > ${PROJECT_DIR}/.gitignore <<EOL
+# IntelliJ project files
+/.idea/
+/*.iml
+
+# Vagrant files
+/*.log
+/.vagrant/
+EOL
--- .dev/provisioning/git-config.sh
+++ .dev/provisioning/git-config.sh
@@ -11,24 +11,5 @@ apt install -y git
 # Config git user
 (cd ${PROJECT_DIR}; git config user.name "${GIT_USER_NAME}" && git config user.email ${GIT_USER_EMAIL})
 
-# Add .gitattributes file
-: '
-When text=auto conversion is enabled in a cross-platform project using push and pull to a central repository, the text
-files containing CRLFs should be normalized.
-When a text file is normalized, its line endings are converted to LF in the repository.
-'
-echo "* text=auto eol=lf" > ${PROJECT_DIR}/.gitattributes
-
-# Add .gitignore file
-cat > ${PROJECT_DIR}/.gitignore <<EOL
-# IntelliJ project files
-/.idea/
-/*.iml
-
-# Vagrant files
-/*.log
-/.vagrant/
-EOL
-
 # Add .gitattributes and .gitignore files to staging area
 (cd ${PROJECT_DIR}; git add .gitignore .gitattributes)
