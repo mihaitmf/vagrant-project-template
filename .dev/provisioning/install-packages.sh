aa50ee0cf833ab54f5005f63bd49d18fd9367649
mihaitmf <mihai.tmf@gmail.com>
2019-04-09 18:07:24 +0300

    Add Terraform installation in vagrant provisioner

--- .dev/provisioning/install-packages.sh
+++ .dev/provisioning/install-packages.sh
@@ -3,7 +3,7 @@
 # Install packages
 apt update
 apt upgrade -y
-apt install -y software-properties-common build-essential htop vim curl net-tools
+apt install -y software-properties-common build-essential htop vim curl net-tools unzip
 
 # Install java and maven
 : '
@@ -45,3 +45,10 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php && php composer
 #python -V
 #python3 -V
 #pip -V
+
+# Install terraform
+#wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+#unzip terraform_0.11.13_linux_amd64.zip
+#mv terraform /usr/local/bin/
+#rm -f terraform_0.11.13_linux_amd64.zip
+#terraform --version
