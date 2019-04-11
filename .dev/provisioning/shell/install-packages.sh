d54d75296ad60205143e62cce33e847def170157
mihaitmf <mihai.tmf@gmail.com>
2019-04-11 20:03:30 +0300

    Add checking if terraform command exists in shell provisioner

--- .dev/provisioning/shell/install-packages.sh
+++ .dev/provisioning/shell/install-packages.sh
@@ -50,8 +50,10 @@ fi
 #pip -V
 
 # Install terraform
-#wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-#unzip terraform_0.11.13_linux_amd64.zip
-#mv terraform /usr/local/bin/
-#rm -f terraform_0.11.13_linux_amd64.zip
+#if ! type terraform > /dev/null; then
+#    wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+#    unzip terraform_0.11.13_linux_amd64.zip
+#    mv terraform /usr/local/bin/
+#    rm -f terraform_0.11.13_linux_amd64.zip
+#fi
 #terraform --version
