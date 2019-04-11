8e93d754bb3b1b1cf74bab40be66427f9cfb4e42
mihaitmf <mihai.tmf@gmail.com>
2019-04-11 17:14:31 +0300

    Fix shell scripts path

--- .dev/provisioning/shell/bootstrap.sh
+++ .dev/provisioning/shell/bootstrap.sh
@@ -4,6 +4,8 @@ PROJECT_DIR=${1}
 GIT_USER_NAME=${2}
 GIT_USER_EMAIL=${3}
 
-./git-config.sh ${PROJECT_DIR} ${GIT_USER_NAME} ${GIT_USER_EMAIL}
-./git-files.sh ${PROJECT_DIR}
-./set-working-dir.sh ${PROJECT_DIR}
+SHELL_SCRIPTS_DIR="${PROJECT_DIR}/.dev/provisioning/shell"
+
+${SHELL_SCRIPTS_DIR}/git-config.sh ${PROJECT_DIR} ${GIT_USER_NAME} ${GIT_USER_EMAIL}
+${SHELL_SCRIPTS_DIR}/git-files.sh ${PROJECT_DIR}
+${SHELL_SCRIPTS_DIR}/set-working-dir.sh ${PROJECT_DIR}
