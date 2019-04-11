6c0dc567c64add4ad130e8bf0a4ada68761a4db1
mihaitmf <mihai.tmf@gmail.com>
2019-04-11 21:15:56 +0300

    By default comment the installation of packages in the vagrant provisioners

--- .dev/provisioning/shell/install-packages.sh
+++ .dev/provisioning/shell/install-packages.sh
@@ -7,17 +7,17 @@ echo "Install commonly used packages"
 apt update >/dev/null
 apt install -y software-properties-common build-essential htop vim curl net-tools unzip >/dev/null
 
-echo "Running install-java.sh"
-${SHELL_SCRIPTS_DIR}/install-java.sh
-
-echo "Running install-php.sh"
-${SHELL_SCRIPTS_DIR}/install-php.sh
-
-echo "Running install-node.sh"
-${SHELL_SCRIPTS_DIR}/install-node.sh
-
-echo "Running install-python.sh"
-${SHELL_SCRIPTS_DIR}/install-python.sh
-
-echo "Running install-terraform.sh"
-${SHELL_SCRIPTS_DIR}/install-terraform.sh
+#echo "Running install-java.sh"
+#${SHELL_SCRIPTS_DIR}/install-java.sh
+#
+#echo "Running install-php.sh"
+#${SHELL_SCRIPTS_DIR}/install-php.sh
+#
+#echo "Running install-node.sh"
+#${SHELL_SCRIPTS_DIR}/install-node.sh
+#
+#echo "Running install-python.sh"
+#${SHELL_SCRIPTS_DIR}/install-python.sh
+#
+#echo "Running install-terraform.sh"
+#${SHELL_SCRIPTS_DIR}/install-terraform.sh
