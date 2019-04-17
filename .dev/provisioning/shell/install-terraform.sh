5ad4984d4c8e089d6291767bcb6550daa8258866
mihaitmf <mihai.tmf@gmail.com>
2019-04-17 12:13:37 +0300

    Fix terraform installation shell script

--- .dev/provisioning/shell/install-terraform.sh
+++ .dev/provisioning/shell/install-terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install terraform if not exists
-if type terraform >/dev/null 2>&1; then
+if ! type terraform >/dev/null 2>&1; then
     wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip >/dev/null 2>&1
     unzip terraform_0.11.13_linux_amd64.zip >/dev/null
     mv terraform /usr/local/bin/
