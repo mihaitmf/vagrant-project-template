a5cf79e27b86bdcadb59313c4fdc94f4aa075fae
mihaitmf <mihai.tmf@gmail.com>
2019-05-22 18:19:18 +0300

    Fix java install script

--- .dev/provisioning/shell/install-java.sh
+++ .dev/provisioning/shell/install-java.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
 JAVA_VERSION=8
+JAVA_HOME_DIR="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64"
+STARTUP_CONFIG_FILE="/etc/profile.d/java.sh"
 
 # Install java
 apt install -y openjdk-${JAVA_VERSION}-jdk >/dev/null
 
 # Setup environment variables
-cat > /etc/profile.d/java.sh <<EOL
-export JAVA_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64
+cat > ${STARTUP_CONFIG_FILE} <<EOL
+export JAVA_HOME=${JAVA_HOME_DIR}
 EOL
 
-java -version
-echo ${JAVA_HOME}
+# Load environment variables
+source ${STARTUP_CONFIG_FILE}
+
+# Check installation
+if ! type java >/dev/null 2>&1; then
+    echo "Error installing java"
+    exit 1
+fi
+if [[ "${JAVA_HOME}" != "${JAVA_HOME_DIR}" ]]; then
+    echo "Error setting JAVA_HOME"
+    exit 1
+fi
+
+#java -version
+#echo ${JAVA_HOME}
