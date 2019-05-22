49b8c3fd2441a2939b4472e5f2c594ee26ea28ec
mihaitmf <mihai.tmf@gmail.com>
2019-05-22 16:46:17 +0300

    Install git only if it not exists

--- .dev/provisioning/shell/git-config.sh
+++ .dev/provisioning/shell/git-config.sh
@@ -4,8 +4,17 @@ PROJECT_DIR=${1}
 GIT_USER_NAME=${2}
 GIT_USER_EMAIL=${3}
 
-apt update >/dev/null
-apt install -y git >/dev/null
+# Install git if not exists
+if ! type git >/dev/null 2>&1; then
+    apt update >/dev/null
+    apt install -y git >/dev/null
+
+    # Check installation succeeded
+    if ! type git >/dev/null 2>&1; then
+        echo "ERROR installing git"
+        exit 1
+    fi
+fi
 
 # Config git user
 (cd ${PROJECT_DIR}; git config user.name "${GIT_USER_NAME}" && git config user.email ${GIT_USER_EMAIL})
