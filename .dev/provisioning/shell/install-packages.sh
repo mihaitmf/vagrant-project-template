04e2960c5b7121ac86e9620c8d6f1078c84e21d0
mihaitmf <mihai.tmf@gmail.com>
2019-11-04 16:30:07 +0200

    Fix shell scripts path because vagrant uses /tmp and it returns some error

--- .dev/provisioning/shell/install-packages.sh
+++ .dev/provisioning/shell/install-packages.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SHELL_SCRIPTS_DIR=$(dirname "$(realpath $0)")
+PROJECT_DIR=${1}
+SHELL_SCRIPTS_DIR="${PROJECT_DIR}/${2}"
 
 echo "Install commonly used packages"
 apt update >/dev/null
